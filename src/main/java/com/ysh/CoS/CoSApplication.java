07d9e0480f38ca239cb7a8c400f0c11e5abf3a44
haneul-99 <hari1213@naver.com>
2024-03-20 16:42:53 +0900

    CoS project
    CoS project

---
+++ src/main/java/com/ysh/CoS/CoSApplication.java
@@ -0,0 +1,13 @@
+package com.ysh.CoS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoSApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ysh/CoS/CoSApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ysh.CoS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoSApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
