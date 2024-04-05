fc57865cdbd2d17dfcb10a037535f20ab10ba312
haneul-99 <hari1213@naver.com>
2024-04-05 23:04:15 +0900

    면접후기 게시판 시작

--- src/main/java/com/ysh/CoS/controller/interviewController.java
+++ src/main/java/com/ysh/CoS/controller/interviewController.java
@@ -1,5 +1,17 @@
 package com.ysh.CoS.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ysh.CoS.service.interviewService;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping(value="/interview")
 public class interviewController {
+	
+	private final interviewService interviewService;
 
 }
