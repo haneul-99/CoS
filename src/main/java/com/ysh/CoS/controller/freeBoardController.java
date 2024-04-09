cbbf9590473bbada7a65512ebac69934f5fb4ce0
SeolHwa Kim <seolhwa1126@naver.com>
2024-04-09 12:39:31 +0900

    게시판 디자인1

--- src/main/java/com/ysh/CoS/controller/freeBoardController.java
+++ src/main/java/com/ysh/CoS/controller/freeBoardController.java
@@ -1,5 +1,23 @@
 package com.ysh.CoS.controller;
 
-public class freeBoardController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.ysh.CoS.service.freeBoardService;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping(value = "/freeBoard")
+public class freeBoardController {
+	
+	//private final freeBoardService service;
+	
+	@GetMapping("/list")
+	public String list() {
+		return "freeBoard/list";
+	}
+	
 }
