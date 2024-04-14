ae5a1b2acc8aab8a13e7270281f5b5809f7ab30f
haneul-99 <hari1213@naver.com>
2024-04-14 14:09:15 +0900

    면접후기게시판 조회

---
+++ src/main/java/com/ysh/CoS/dto/interviewDTO.java
@@ -0,0 +1,16 @@
+package com.ysh.CoS.dto;
+
+import lombok.Data;
+
+@Data
+public class interviewDTO {
+	private String iSeq;		//면접후기 번호
+	private String mSeq;		//회원번호 
+	private String iTitle;		//제목 
+	private String iContent;	//내용 
+	private String iDate;		//작성일 
+	private String iCount;		//조회수
+	private String iFile;		//첨부파일
+	
+	private String name;		//회원이름
+}
